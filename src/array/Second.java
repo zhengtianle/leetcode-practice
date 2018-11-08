b8fa4bcb1f0072bd250a99839c69a1c922f3b4dd
zhengtianle <982978764@qq.com>
2018-11-08 23:16:43 +0800

    add the array folder

---
+++ src/array/Second.java
@@ -0,0 +1,38 @@
+package array;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Time 18-11-8
+ * @Author ZhengTianle
+ * Description:
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/22/
+ */
+public class Second {
+
+    public static int maxProfit(int[] prices) {
+        int money = 0;  //利润
+        boolean hasBought = false;  //是否买入标志位，初始值为未买入
+        for(int i = 0; i < prices.length - 1; i++){
+            //买入
+            if(prices[i] < prices[i + 1] && !hasBought){
+                money -= prices[i];
+                hasBought = true;
+            }
+            //卖出
+            if(hasBought & prices[i] > prices[i + 1]){
+                money += prices[i];
+                hasBought = false;
+            }
+        }
+        if(hasBought){
+            money += prices[prices.length - 1];
+        }
+
+        return money;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+    }
+}
