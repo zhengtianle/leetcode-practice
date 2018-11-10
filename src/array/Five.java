ae6ad3b108d5ad083945e2fb6d74bbf2d3166efe
zhengtianle <982978764@qq.com>
2018-11-10 23:52:36 +0800

    add array.Five.java

---
+++ src/array/Five.java
@@ -0,0 +1,37 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Time 18-11-10
+ * @Author ZhengTianle
+ * Description:
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/25/
+ */
+public class Five {
+
+    public static int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        boolean repeat = false;
+        int i;
+        for(i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i - 1]) {
+                repeat = true;
+            } else {
+                if(repeat == false){
+                    break;
+                } else {
+                    repeat = false;
+                }
+            }
+        }
+
+        return nums[i - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(singleNumber(new int[]{4,1,2,1,2}));
+    }
+}
