e330e492f604671fadbc86a64ca16c2a3fd3313c
zhengtianle <982978764@qq.com>
2018-11-15 09:20:12 +0800

    add string.First.java and string.Third.java

---
+++ src/string/First.java
@@ -0,0 +1,35 @@
+package string;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Time 18-11-15
+ * @Author ZhengTianle
+ * Description:
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/32/
+ */
+public class First {
+    /*StringBuilder解法---慢*/
+    public static String reverseString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = s.length() - 1; i >= 0; i--){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    /*char[]解法---快*/
+    public static String anotherReverseString(String s){
+        char[] result = new char[s.length()];
+        int j = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            result[j++] = s.charAt(i);
+        }
+        return new String(result);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("A man, a plan, a canal: Panama"));
+        System.out.println(anotherReverseString("A man, a plan, a canal: Panama"));
+    }
+}
---
+++ src/string/Third.java
@@ -0,0 +1,49 @@
+package string;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Time 18-11-15
+ * @Author ZhengTianle
+ * Description:
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/34/
+ */
+public class Third {
+
+    public static int firstUniqChar(String s) {
+        int len = s.length();
+        char[] words = s.toCharArray();
+        boolean repeated = false;
+        for(int i = 0; i < len; i++){
+            repeated = false;
+            for(int j = 0; j < len; j++){
+                if(i == j)
+                    continue;
+                if(words[i] == words[j]){
+                    repeated = true;
+                    break;
+                }
+            }
+            if(!repeated){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public static int anotherFirstUniqChar(String s){
+        int len = s.length();
+        for(int i = 0; i < len; i++){
+            if(s.lastIndexOf(s.charAt(i)) == s.indexOf(s.charAt(i))){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("lovveleettcocdde"));
+        System.out.println(anotherFirstUniqChar("z"));
+    }
+}
