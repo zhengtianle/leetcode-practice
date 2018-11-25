484859e0eff42b06e9d7e9bc4d12ab0b95519616
zhengtianle <982978764@qq.com>
2018-11-25 23:32:31 +0800

    add one solution for TlinkedList.Third.java

--- src/linkedList/Third.java
+++ src/linkedList/Third.java
@@ -11,10 +11,37 @@ package linkedList;
 public class Third {
 
     public static ListNode reverseList(ListNode head) {
+        if(head == null)
+            return null;
+        ListNode listNode = new ListNode(head.val);
+        head = head.next;
+        while(head != null){
+            ListNode p = new ListNode(head.val);
+            p.next = listNode;
+            listNode = p;
+            head = head.next;
+        }
+        return listNode;
+    }
 
+    public static void printList(ListNode head){
+        while(head != null){
+            System.out.print(head.val + "->");
+            head = head.next;
+        }
+        System.out.println("null");
     }
 
     public static void main(String[] args) {
-
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        printList(reverseList(listNode1));
     }
 }
