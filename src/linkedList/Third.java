a616834bc6aa17721be0677709262fb1aeef6059
zhengtianle <982978764@qq.com>
2018-11-27 09:53:17 +0800

    linkedList.Third.java over

--- src/linkedList/Third.java
+++ src/linkedList/Third.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -10,6 +12,7 @@ package linkedList;
  */
 public class Third {
 
+    /*耗时多、耗空间多*/
     public static ListNode reverseList(ListNode head) {
         if(head == null)
             return null;
@@ -22,6 +25,33 @@ public class Third {
             head = head.next;
         }
         return listNode;
+    }
+
+    /*耗时少、占空间少，只额外增加了标签，没有增加内存*/
+    public static ListNode reverseList1(ListNode head){
+        if(head == null)
+            return null;
+
+        ListNode listNode = head;
+        head = head.next;
+        listNode.next = null;
+        while(head != null){
+            ListNode listNode1 = head;
+            head = head.next;
+            listNode1.next = listNode;
+            listNode = listNode1;
+        }
+        return listNode;
+    }
+
+    /*递归实现*/
+    public static ListNode reverseList2(ListNode head){
+        if(head == null || head.next == null)//链表为空直接返回，而head->next为空是递归终点
+            return head;
+        ListNode listNode =  reverseList2(head.next);//一直递归到链尾
+        head.next.next = head;//翻转链表指向
+        head.next = null;//防止环形链表
+        return listNode;//返回当前翻转链表的头
     }
 
     public static void printList(ListNode head){
@@ -42,6 +72,6 @@ public class Third {
         listNode2.next = listNode3;
         listNode3.next = listNode4;
         listNode4.next = listNode5;
-        printList(reverseList(listNode1));
+        printList(reverseList2(listNode1));
     }
 }
