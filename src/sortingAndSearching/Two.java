76ec9df37a2568f0eb9fd97f7a3f004b77e13a5a
zhengtianle <982978764@qq.com>
2018-12-03 19:15:33 +0800

    trees.Five.java and sortingAndSearching

---
+++ src/sortingAndSearching/Two.java
@@ -0,0 +1,31 @@
+package sortingAndSearching;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Time 18-12-3
+ * @Author ZhengTianle
+ * Description:
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/8/sorting-and-searching/53/
+ */
+public class Two extends VersionControl{
+    public int firstBadVersion(int n) {
+        long low = 1;
+        long high = n;
+        long mid = (low + high) >> 1;
+        while(low <= high){
+            if(!isBadVersion((int)mid))
+                low = mid + 1;
+            else
+                high = mid - 1;
+            mid = (low + high) >> 1;
+        }
+
+        return (int)low;
+    }
+
+    public static void main(String[] args) {
+        Two two = new Two();
+        System.out.println(two.firstBadVersion(2126753390));
+    }
+}
---
+++ src/trees/Five.java
@@ -0,0 +1,24 @@
+package trees;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Time 18-12-3
+ * @Author ZhengTianle
+ * Description:
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/51/
+ */
+public class Five {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length == 0)
+            return null;
+        TreeNode node = new TreeNode(nums[nums.length/2]);
+        if(nums.length != 1){
+            node.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, nums.length/2));
+            node.right = sortedArrayToBST(Arrays.copyOfRange(nums, nums.length/2 + 1, nums.length));
+        }
+        return node;
+    }
+}
