77fa583b2733acb53513dadcedb999462a1dd4e3
zhengtianle <982978764@qq.com>
2019-01-14 22:38:55 +0800

    perfect Third.java

--- src/topInterviewQuestionsMedium/backtracking/Third.java
+++ src/topInterviewQuestionsMedium/backtracking/Third.java
@@ -50,15 +50,46 @@ public class Third {
     }
 
     /**
-     * 递归解法
+     * 递归交换解法（与当前元素后面的交换）
      */
-    public void fullArray1(){
+    public List<List<Integer>> permute1(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0){
+            return result;
+        }
 
+        List<Integer> list = new ArrayList<>();
+        fullArray1(result, list, nums, 0);
+        return result;
+    }
+    public void fullArray1(List<List<Integer>> result, List<Integer> list, int[] nums, int count){
+        if(count == nums.length){
+            List<Integer> list1 = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++){
+                list1.add(nums[i]);
+            }
+            result.add(list1);
+            return;
+        }
+
+        for(int i = count; i < nums.length; i++){
+            swap(nums, i, count);
+            fullArray1(result, list, nums, count+1);
+            swap(nums, i, count);
+        }
+    }
+    public void swap(int[] nums, int x, int y){
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
     }
 
     /**
      * 遍历解法
      */
+    public List<List<Integer>> permute2(int[] nums) {
+
+    }
     public void fullArray2(){
 
     }
