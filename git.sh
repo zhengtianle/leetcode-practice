8007657434c66880d1bc96ecc2f3834a9e658acd
zhengtianle <982978764@qq.com>
2019-01-22 18:38:14 +0800

    gitshell

---
+++ git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /home/zhengtianle/workspace/idea-workspace/LeetCodePractice
+echo "-------------------add commit pull push 一步到位："
+git add .
+git status
+read -p "输入commit注释：" input
+if [ -n "${input}" ]
+then
+    git commit -m "${input}"
+    git pull
+    git status
+    git push origin master
+else
+    read -p "您没有输入commit注释，是否重新运行此脚本 y/n？" choice
+    if [ ${choice}x = "y" ]
+    then
+        ./git.sh
+    fi
+fi
